267dbd4e9e93884e772ebb96bf2329d56887bcd3
Christianah613 <Christianah613@gmail.com>
2022-12-06 10:49:25 +0000

    0-print_list_integer.py

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -6,7 +6,7 @@
 * @n: integer
 * @next: points to the next node
 * Description: singly linked list node structure
-* for Holberton project
+* for project
 */
 typedef struct listint_s
 {
@@ -17,5 +17,5 @@ listint_t;
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-listint_t *insert_node(listint_t **head, int number);
+int is_palindrome(listint_t **head);
 #endif /* LISTS_H */
